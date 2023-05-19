1d7a0f87d0b7dd656b6687b76b34db9f71f37bce
Pawan <kumarpawanm8085@gmail.com>
2023-05-19 23:51:37 +0530

    selection sort java

---
+++ Array/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+    
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swapping the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+}
