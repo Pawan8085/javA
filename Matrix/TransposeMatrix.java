3e759de8187fa6e76c49b5ef1fc8bcc785940ceb
Pawan <kumarpawanm8085@gmail.com>
2023-07-10 18:12:28 +0530

    transpose matrix

---
+++ Matrix/TransposeMatrix.java
@@ -0,0 +1,19 @@
+package javA.Matrix;
+
+public class TransposeMatrix {
+    
+    public void transpose(int n,int matrix[][])
+    {
+        //GFG : Transpose Matrix 
+        
+        for(int i=0;i<n;++i){
+           
+            for(int j=0;j<=i;++j){
+                 int temp = matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp;
+            }
+        }
+        
+    }
+}
