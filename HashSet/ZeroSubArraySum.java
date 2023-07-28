04fc876f1282dd8b3a14abbc9d846e8fe7c106ff
Pawan <kumarpawanm8085@gmail.com>
2023-07-28 22:49:40 +0530

    longest subarray with zero sum

---
+++ HashSet/ZeroSubArraySum.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class ZeroSubArraySum {
+    // max subarray with zero sum
+     int maxLen(int arr[], int n){
+       
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int max = 0;
+        int sum = 0;
+        
+        // this is for 0 subarray conditon
+        map.put(0 , -1);
+        
+        for(int i=0;i<n;++i){
+            sum+=arr[i];
+            
+            if(map.containsKey(sum)){
+                
+                int len = i-map.get(sum);
+                max = Math.max(len, max);
+            }
+            else{
+                
+                map.put(sum, i);
+                
+            }
+        }
+        return max;
+    }
+}
